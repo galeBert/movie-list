f0bfa5a9c8d3bd0122174bd551d9a800fea2755e
galeBert <76025267+galeBert@users.noreply.github.com>
2024-02-23 02:58:15 +0700

    feat(dx): fetch 2

--- app/api/search/route.tsx
+++ app/api/search/route.tsx
@@ -5,9 +5,8 @@ export async function GET(
   req: NextRequest,
   { params }: { params: { storeId: string } }
 ) {
+  const query = req.nextUrl.searchParams.get("q");
   try {
-    const query = req.nextUrl.searchParams.get("q");
-
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
     const token = process.env.NEXT_API_TOKEN;
     const movies = await fetch(url, {
