ec053fcba9f5098556717683ca98c7127e31157f
galeBert <76025267+galeBert@users.noreply.github.com>
2024-02-23 02:44:33 +0700

    feat(dx): suspense

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import SearchResult from "./components/search-result";
 
 export default function Search() {
-  return <SearchResult />;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <SearchResult />
+      </Suspense>
+    </div>
+  );
 }
